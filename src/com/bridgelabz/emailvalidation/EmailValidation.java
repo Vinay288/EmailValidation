59dfb675287153de6d15764ad04c47f264ad4b0c
Vinay288 <vinayhiremath288@gmail.com>
2021-09-08 01:36:43 +0530

    ADDED: logic to allow specofoc special characters in email

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -6,20 +6,19 @@ import java.util.regex.Pattern;
 public class EmailValidation {
 
 	public static boolean emailValidation(String emailAddress) {
-		String emailRegex = "^[a-zA-Z]{1,}[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2,}";
-		return Pattern.matches(emailRegex, emailAddress);
+		String emailRegex = "^[a-zA-Z]{1,}[\\.\\_\\-\\+]?[a-zA-Z]*[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2,}";
+		return Pattern.matches(emailRegex,emailAddress);
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
+		Scanner scanner =new Scanner(System.in);
+		
 		System.out.println("Enter email");
-		String email = scanner.nextLine();
-		while (!emailValidation(email)) {
+		String email=scanner.nextLine();
+		while(!emailValidation(email)) {
 			System.out.println("Invalid format. Enter again");
-			email = scanner.nextLine();
+			email=scanner.nextLine();
 		}
-
+		
 	}
-
 }
